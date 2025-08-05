e9a8fb465db5613b35862dd187d991c378ff0b34
gustavfaustino <gustav.faustino107@gmail.com>
2025-08-05 19:11:03 -0300

    substitui a lógica de verificação do número secreto para um loop, melhorando a interação do usuário

--- Logica JS/logica-js-projeto_inicial/app.js
+++ Logica JS/logica-js-projeto_inicial/app.js
@@ -3,9 +3,11 @@ let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+while (chute != numeroSecreto) {
+    if (chute < 1 || chute > 10) {
+        alert('Número inválido! Escolha um número entre 1 e 10.');
+    } else {
+        alert('Você errou! Tente novamente.');
+    }
+    chute = prompt('Escolha um número entre 1 e 10');
 }
