9518659a788849f97643ce33ff5c500f21fbfa01
gustavfaustino <gustav.faustino107@gmail.com>
2025-08-11 14:59:56 -0300

    adiciona verificação de número inválido e reorganiza a lógica de interação no jogo do número secreto

--- Logica JS/logica-js-projeto_inicial/app.js
+++ Logica JS/logica-js-projeto_inicial/app.js
@@ -1,13 +1,16 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
 
 while (chute != numeroSecreto) {
-    if (chute < 1 || chute > 10) {
-        alert('Número inválido! Escolha um número entre 1 e 10.');
-    } else {
-        alert('Você errou! Tente novamente.');
-    }
     chute = prompt('Escolha um número entre 1 e 10');
+    if (chute == numeroSecreto) {
+        alert('Parabéns! Você acertou o número secreto.');
+    } else {
+       if (chute < numeroSecreto) {
+         alert('Você errou! O número secreto é maior que ' + chute);
+       } else {
+         alert('Você errou! O número secreto é menor que ' + chute);
+       }
+    }
 }
