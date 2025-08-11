ce06ec09e0da8529549b1a5d5036173ba56d5a3b
gustavfaustino <gustav.faustino107@gmail.com>
2025-08-11 15:57:41 -0300

    refatora a lógica de interação no jogo do número secreto para melhorar a legibilidade e a estrutura do código

--- Logica JS/logica-js-projeto_inicial/app.js
+++ Logica JS/logica-js-projeto_inicial/app.js
@@ -5,16 +5,21 @@ let tentativas = 1
 let chute;
 
 while (chute != numeroSecreto) {
-    chute = Number(prompt('Digite um número entre 1 e 10:'));
-    if (chute == numeroSecreto) {
-        alert('Parabéns! Você acertou o número secreto em ' + tentativas + ' tentativas!');
+  chute = Number(prompt('Digite um número entre 1 e 10:'));
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute < numeroSecreto) {
+      alert('Você errou! O número secreto é maior que ' + chute);
     } else {
-       if (chute < numeroSecreto) {
-         alert('Você errou! O número secreto é maior que ' + chute);
-       } else {
-         alert('Você errou! O número secreto é menor que ' + chute);
-       }
-       tentativas++
+      alert('Você errou! O número secreto é menor que ' + chute);
     }
-    // console.log("T:"+tentativas)
+    tentativas++
+  }
+  // console.log("T:"+tentativas)
+}
+if (tentativas == 1) {
+  alert('Uau! Você acertou de primeira! O número secreto é ' + numeroSecreto);
+} else {
+  alert('Parabéns! Você acertou o número secreto em ' + tentativas + ' tentativas!');
 }
