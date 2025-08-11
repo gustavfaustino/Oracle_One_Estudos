babe425bd5ed423d66213fc49677e3f2f5c1d2f9
gustavfaustino <gustav.faustino107@gmail.com>
2025-08-11 15:04:19 -0300

    melhora a lógica do jogo do número secreto, tornando o número secreto aleatório e adicionando contagem de tentativas

--- Logica JS/logica-js-projeto_inicial/app.js
+++ Logica JS/logica-js-projeto_inicial/app.js
@@ -1,16 +1,18 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 console.log(numeroSecreto)
+let tentativas = 1
 
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 10');
     if (chute == numeroSecreto) {
-        alert('Parabéns! Você acertou o número secreto.');
+        alert('Parabéns! Você acertou o número secreto em ' + tentativas + ' tentativas!');
     } else {
        if (chute < numeroSecreto) {
          alert('Você errou! O número secreto é maior que ' + chute);
        } else {
          alert('Você errou! O número secreto é menor que ' + chute);
        }
+       tentativas++
     }
 }
