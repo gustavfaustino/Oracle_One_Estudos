2dc4b8415180bc90ab08bd2ca22335f7e92810ec
gustavfaustino <gustav.faustino107@gmail.com>
2025-09-11 16:55:02 -0300

    refatora a função de troca de texto para melhorar a legibilidade e a estrutura do código

--- Logica JS/logica-js-projeto_inicial/app.js
+++ Logica JS/logica-js-projeto_inicial/app.js
@@ -1,3 +1,8 @@
+function trocarTextoCampo(novoTexto, tag) {
+    let campo = document.getElementById(tag);
+    campo.innerHTML = novoTexto;
+}
+
 // let titulo = document.getElementById('titulo');
 // titulo.innerHTML = 'Jogão do número secreto';
 trocarTextoCampo('Jogão do número secreto', 'titulo');
@@ -7,19 +12,23 @@ trocarTextoCampo('Jogão do número secreto', 'titulo');
 trocarTextoCampo('Adivinhe o número entre 1 e 10', 'descricao');
 
 let numero_secreto = parseInt(Math.random() * 10) + 1;
+let tentativas = 1;
 
 function verificarChute() {
     let chute = parseInt(document.querySelector('.container__input').value);
-
+   
     if (chute == numero_secreto) {
-        titulo.innerHTML = 'Você acertou!';
+        let mensagem_tentativas = `Você acertou em ${tentativas} ${tentativas > 1 ? 'tentativas' : 'tentativa'}!`;
+
+        trocarTextoCampo('Você acertou!', 'titulo');
+        trocarTextoCampo(mensagem_tentativas, 'descricao');
         titulo.style.color = 'green';
     } else {
-        titulo.innerHTML = 'Você errou! Tente novamente.';
+        if (chute > numero_secreto) {
+            trocarTextoCampo('O número secreto é menor!', 'descricao');
+        } else {
+            trocarTextoCampo('O número secreto é maior!', 'descricao');
+        }
+        tentativas++;
     }
-}
-
-function trocarTextoCampo(novoTexto, tag){
-    let campo = document.getElementById(tag);
-    campo.innerHTML = novoTexto;
 }
