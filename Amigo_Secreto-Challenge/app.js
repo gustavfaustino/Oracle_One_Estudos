87dc6a395bd19e61bdf530138560341ec8b498df
gustavfaustino <gustav.faustino107@gmail.com>
2025-09-17 18:48:04 -0300

    Refatorar função atualizarLista para adicionar amigos à lista de forma dinâmica

--- Amigo_Secreto-Challenge/app.js
+++ Amigo_Secreto-Challenge/app.js
@@ -13,4 +13,20 @@ function adicionarAmigo() {
     }
     // Verificação de amigos adicionados
     console.log("Amigos:", amigos);
+
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista_amigos = document.getElementById("listaAmigos");
+
+    // Limpar a lista atual
+    lista_amigos.innerHTML = "";
+
+    // Adicionar cada amigo à lista
+    amigos.forEach(function(amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista_amigos.appendChild(li);
+    });
 }
