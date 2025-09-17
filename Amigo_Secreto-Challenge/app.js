988a8ac6f61a483dd80092b0d59092b71e7f358d
gustavfaustino <gustav.faustino107@gmail.com>
2025-09-17 18:43:49 -0300

    Criado uma primeira versão para adicionar amigo

--- Amigo_Secreto-Challenge/app.js
+++ Amigo_Secreto-Challenge/app.js
@@ -1,2 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+
+function adicionarAmigo() {
+    let nome_amigo = document.getElementById("amigo").value;
+
+    if (nome_amigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    } else {
+        amigos.push(nome_amigo);
+        document.getElementById("amigo").value = "";
+    }
+    // Verificação de amigos adicionados
+    console.log("Amigos:", amigos);
+}
